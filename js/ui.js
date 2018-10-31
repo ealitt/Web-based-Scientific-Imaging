d954543c2b21f684a565148a12aaa1288177eb7a
Eammon <ealitt@gmail.com>
2018-10-31 11:09:16 -0400

    Added image selection capability, color selection tool, and slider

--- js/ui.js
+++ js/ui.js
@@ -1,8 +1,42 @@
+var imageNum = 0;
+
 $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
     });
+    $('.dropper').dropper({
+      clickCallback: function(color) {
+        $('#swatch').css('background-color','#'+color.rgbhex);
+      }
+    });
 });
+
+function initSlider(){
+  $(document).ready( function() {
+    $('#threshold').slider();
+
+    $(document).on("click","#page-slider",function(){
+        $('#threshold').toggle("slow");
+    });
+  });
+  console.log("initialized?");
+}
+
+function tools(){
+  $( "#threshold" ).slider({
+    value: 0,
+    animate:"fast",
+    orientation: "horizontal",
+    slide: function( event, ui ) {
+      console.log($( "#threshold" ).slider( "value" ));
+    }
+  });
+}
+
+function imageSelect(imgVal) {
+  var selectedImage = document.getElementById("edited");
+  selectedImage.src = imgVal.src;
+}
 
 function uploadFile() {
   // let source = document.getElementById('upload');
@@ -16,7 +50,7 @@ function uploadFile() {
       $('#files').on("change", function(event) {
           var files = event.target.files; //FileList object
           var output = document.getElementById("result");
-          for(var i = 0; i< files.length; i++)
+          for(var i = 0; i < files.length; i++)
           {
               var file = files[i];
 
@@ -26,11 +60,15 @@ function uploadFile() {
                     imgReader.addEventListener("load",function(event){
                         var imgFile = event.target;
                         var div = document.createElement("div");
-                        div.innerHTML = "<img class='thumbnail' src='" + imgFile.result + "'" +
-                                "title='preview image'/>";
-                        // div.innerHTML = "<option data-img-src='" + imgFile.result + "'" +
-                        //         " value='" + i + "'><option/>";
+                        div.innerHTML = "<img id=image_" + imageNum + " class='thumbnail' src='" + imgFile.result + "'" +
+                                "title='preview image' onclick=imageSelect(image_" + imageNum + ") />";
                         output.insertBefore(div,null);
+                        if(imageNum == 0){
+                          let selectedImage = document.getElementById("edited");
+                          let defaultImage = document.getElementById("image_0");
+                          selectedImage.src = defaultImage.src;
+                        }
+                        imageNum = imageNum + 1;
                   });
                   //Read the image
                   $('#clear, #result').show();
@@ -44,7 +82,6 @@ function uploadFile() {
               $(this).val("");
           }
           }
-
       });
   }
   else
@@ -66,8 +103,18 @@ $('#clear').on("click", function() {
     $(this).hide();
 });
 
+// $('#ex1').each(function(){
+//   $(this).slider({
+//   	formatter: function(value) {
+//   		return 'Current value: ' + value;
+//   	}
+//   })
+// });
+
 // $('.image-picker').imagepicker();
 
 window.onload = function() {
+  initSlider();
   uploadFile();
+  tools();
 }
